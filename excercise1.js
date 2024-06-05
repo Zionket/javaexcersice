2886d792881fa06d82299ed5724512e421eb4475
Zionket <ziontown19@gmail.com>
2024-06-05 19:47:21 +0300

    ksl

--- excercise1.js
+++ excercise1.js
@@ -1,5 +1,5 @@
 let fName = "Tsion";
 let Lname = "Ketema";
 const age = 13;
-let address ="Addis Ababa";
-console.log(`my name is ${fName} ${Lname}.i am ${age} years old and i live in the   ${address} `)
+let address = "Addis Ababa";
+console.log(`my name is ${fName} ${Lname}.i am ${age} years old and i live in the beutiful  ${address} `)
