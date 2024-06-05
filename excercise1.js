5b1791180b5eed91e1d246c0ee0637673b27c5e7
Zionket <158601897+Zionket@users.noreply.github.com>
2024-06-05 20:36:03 +0300

    Update excercise1.js

--- excercise1.js
+++ excercise1.js
@@ -2,4 +2,4 @@ let fName = "Tsion";
 let Lname = "Ketema";
 const age = 13;
 let address = "Addis Ababa";
-console.log(`my name is ${fName} ${Lname}.i am ${age} years old and i live in the beutiful  ${address} `)
+console.log(`my name is ${fName} ${Lname}.i am ${age} years old and i live in the beutiful  ${address} and edited `)
