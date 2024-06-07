4c25009c7df16bec1af1abaab09dd9666ec0e613
Zionket <ziontown19@gmail.com>
2024-06-07 20:22:13 +0300

    excercise 5 added

---
+++ excercise5.js
@@ -0,0 +1,10 @@
+//excercise 5 sone by Tsion
+let studentAge = 18;
+let hasDiploma = true;
+let examScore = 70;
+if(studentAge >= 18 && hasDiploma && examScore >=80 ){
+    console.log(`Congrats you are eligible`);
+}
+else {
+    console.log(`Sorry you are not eligible`);
+}
