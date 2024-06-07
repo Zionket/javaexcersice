90b3473c75f4af63f24e64eef3deafb9da5c3fd8
Zionket <ziontown19@gmail.com>
2024-06-07 14:57:33 +0300

    1n2 added

---
+++ excercise2.js
@@ -0,0 +1,6 @@
+let c = 20;
+let f = 30;
+let toFaranite = c * 9/5 + 32;
+let toCelcius = (f-32) * 5/9;
+console.log(`20c in faranite is = ${toFaranite}`);
+console.log( `30f in celcius is = ${toCelcius}`);
