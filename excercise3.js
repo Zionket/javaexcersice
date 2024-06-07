0d060627db4ca2a4039df07359f8f38e1a4aab24
Zionket <ziontown19@gmail.com>
2024-06-07 16:26:20 +0300

    updated ther times

---
+++ excercise3.js
@@ -0,0 +1,16 @@
+//excercise 3 by Tsion
+let age = 15;
+if(age < 13)
+{
+    console.log(`He/she is a child`);
+}
+    else if(age >= 13 && age <= 19)
+    {
+        console.log("He/she is a Teenager");
+    }
+    else if(age >= 20 && age <= 59)
+    {
+        console.log("He/she is an Adult");
+    }
+    else
+    console.log("He/she is a Senior");
