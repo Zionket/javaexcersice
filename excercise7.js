3a3ec65dd594135ad2a0ab5193cc1963622f7352
Zionket <ziontown19@gmail.com>
2024-06-09 19:54:37 +0300

    excercise 7 add

---
+++ excercise7.js
@@ -0,0 +1,27 @@
+let dayNumber = 9;
+let dayName;
+switch (dayNumber) {
+    case 1:
+      console.log("Monday");
+      break;
+    case 2:
+      console.log("Tuesday");
+      break;
+    case 3:
+      console.log("Wednesday");
+      break;
+    case 4:
+      console.log("Thursday");
+      break;
+    case 5:
+      console.log("Friday");
+      break;
+    case 6:
+      console.log("Saturday");
+      break;
+    case 7:
+      console.log("Sunday");
+      break;
+    default:
+      console.log("Error: Input must be a number between 1 and 7.");
+  }
