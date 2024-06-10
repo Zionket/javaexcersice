4e314c07dc0e3f8f42965b353cb199a7181156a3
Zionket <ziontown19@gmail.com>
2024-06-10 15:26:22 +0300

    added

---
+++ excercise7t.js
@@ -0,0 +1,19 @@
+let fruit = 1;
+let fruitType;
+switch(fruit)
+{
+    case 1:
+        fruitType = "banana";
+        break;
+    case 2:
+        fruitType = "orange";
+        break;
+    case 2:
+        fruitType = "apple";
+        break;
+    default:
+        console.log("error");
+        break;
+
+}
+console.log(`Its a ${fruitType}`);
