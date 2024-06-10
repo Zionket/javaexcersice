6d65e51b56b0a13848ffc00c0cc31a62ed2027c3
Zionket <ziontown19@gmail.com>
2024-06-10 15:37:53 +0300

    added again

--- excercise7t.js
+++ excercise7t.js
@@ -1,19 +1,16 @@
-let fruit = 1;
-let fruitType;
-switch(fruit)
-{
-    case 1:
-        fruitType = "banana";
-        break;
-    case 2:
-        fruitType = "orange";
-        break;
-    case 2:
-        fruitType = "apple";
-        break;
-    default:
-        console.log("error");
-        break;
+// excecise7.2
+let fruit = 'banana';
 
+switch (fruit) {
+  case 'banana':
+    console.log("Bananas are yellow and sweet.");
+    break;
+  case 'apple':
+    console.log("Apples come in many varieties and colors.");
+    break;
+  case 'orange':
+    console.log("Oranges are citrus fruits that are rich in vitamin C.");
+    break;
+  default:
+    console.log("I don't have any information about that fruit.");
 }
-console.log(`Its a ${fruitType}`);
