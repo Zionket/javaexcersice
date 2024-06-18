6749700e1f87bf44fbbac0b71bcbc164a3a380ab
Zionket <ziontown19@gmail.com>
2024-06-18 11:14:07 +0300

    added

---
+++ culculateareaperimeter.js
@@ -0,0 +1,41 @@
+const pi = 3.14;
+function rectangelArea(l,w){
+    if(isNaN(l) || isNaN(w) || l<=0 || w<=0)
+    {
+        return "invalid input";
+    }
+    return l*w;
+}
+function rectangelPerimeter(l,w){
+    if(isNaN(l) || isNaN(w) || l<=0 || w<=0){
+        return "invalid input";
+
+    }
+    return 2*l*w;
+
+}
+function circeleArea (r){
+    if(isNaN(r) || r <=0){
+
+        return "invalid input";
+    }
+    return pi*Math.pow(r,2);
+}
+
+function circleCircumference(r){
+
+    if(isNaN(r) || r <= 0){
+        return "invalid input";
+    }
+    return r*pi*2;
+}
+
+const rectangelarearesult = rectangelArea(2,3);
+const rectangelperimeterresult = rectangelPerimeter(2,3);
+const circelearearesult = circeleArea(4);
+const circlecurcumferencereult = circleCircumference(4)
+
+console.log(`Area = ${rectangelarearesult}`)
+console.log(`perimeter = ${rectangelperimeterresult}`)
+console.log(`perimeter = ${circelearearesult}`);
+console.log(`circumfernce = ${circlecurcumferencereult}`);
