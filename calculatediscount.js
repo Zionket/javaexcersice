27e76d1361ccb00fb19ed2aed1a13be86783094a
Zionket <ziontown19@gmail.com>
2024-06-19 15:56:22 +0300

    calculatediscount is added

---
+++ calculatediscount.js
@@ -0,0 +1,10 @@
+function calculatediscountprice (op ,dp){
+let  discountamaunt = op*dp/100
+let discountPrice  = op - discountamaunt;
+
+return discountPrice;
+
+}
+
+const result = calculatdiscountprice(100,);
+console.log(`The final price is ${result}`);
